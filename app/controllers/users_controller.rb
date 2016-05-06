86d403c37fd1c58581eb3e5cdc482f426147d2d1
rudis <rudislpp@gmail.com>
2016-05-06 02:38:03 -0300

    Add sing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -12,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Bem vindo ao Blog da Nanda, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -48,8 +50,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
   end
+
   def require_same_user
-    if  current_user != @user
+    if current_user != @user
       flash[:danger] = " Você só pode editar sua própria conta"
       redirect_to root_path
     end
