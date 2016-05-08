975864549a4dd02468efae7b9aca7b955cf683dd
rudis <rudislpp@gmail.com>
2016-05-08 17:25:14 -0300

    add admin user func

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController <ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "Bem-vindo ao blog #{user.username}!" #tirar o @ para pegar o nome do usuario
+      flash[:success] = "Bem-vindo ao blog da nanda, #{user.username}!" #tirar o @ para pegar o nome do usuario
       redirect_to user_path(user)
     else
       flash.now[:danger]= "Erro ao tentar logar!"
